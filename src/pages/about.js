1d4e479ba647d14c39037920da4c750fab3cdfdd
sarwar-asik <sarwarasik@gmail.com>
2023-07-19 17:37:38 +0600

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ABoutPage = () => {
+  return (
+    <div>
+      <h2>This is about page</h2>
+    </div>
+  );
+};
+
+export default ABoutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h2>This is Contact Page</h2>
+        </div>
+    );
+};
+
+export default ContactPage;
