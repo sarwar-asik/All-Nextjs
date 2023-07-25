12d4d71334c6f77ea78abc9bc0d5c519fd74af35
sarwar-asik <sarwarasik@gmail.com>
2023-07-25 12:57:43 +0600

    use client

---
+++ ALLMD/useClient.md
@@ -0,0 +1,6 @@
+## for get console data in client side  or browser 
+
+            "use client"
+
+use it top of a router or pages 
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
-import React from "react";
+"use client"
 
 const HomePage = () => {
+  console.log("Check Server ")
+
   return (
     <div>
       <h1 className="text-center text-4xl ">This is Home page</h1>
