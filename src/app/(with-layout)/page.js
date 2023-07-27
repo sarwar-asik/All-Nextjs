1c70ecc6bfe9bf5cac2a8e7888e2580e71ee2392
sarwar-asik <sarwarasik@gmail.com>
2023-07-27 14:43:39 +0600

    noted layout

--- src/app/(with-layout)/page.js
+++ src/app/(with-layout)/page.js
@@ -5,7 +5,7 @@ const HomePage = () => {
 
   return (
     <div>
-      <h1 className="text-center text-4xl ">This is Home page</h1>
+      <h1 className="text-center text-4xl ">This is Home pagess</h1>
     
     </div>
   );
